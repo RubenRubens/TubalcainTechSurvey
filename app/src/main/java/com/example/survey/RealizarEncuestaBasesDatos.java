e5a901283926b853fdcbb0381ed537246062c561
Ruben Arriazu <ruben.arsi@protonmail.ch>
2022-02-06 18:20:25 +0100

    Soluciona bug del activity Bases de Datos

--- app/src/main/java/com/example/survey/RealizarEncuestaBasesDatos.java
+++ app/src/main/java/com/example/survey/RealizarEncuestaBasesDatos.java
@@ -2,7 +2,11 @@ package com.example.survey;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class RealizarEncuestaBasesDatos extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class RealizarEncuestaBasesDatos extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.realizar_encuesta_bd_activity);
+
+
+        MaterialButton siguiente = (MaterialButton) findViewById(R.id.siguiente);
+        siguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RealizarEncuestaBasesDatos.this, RealizarEncuestaWeb.class));
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/survey/RealizarEncuestaLenguajes.java
+++ app/src/main/java/com/example/survey/RealizarEncuestaLenguajes.java
@@ -2,7 +2,11 @@ package com.example.survey;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class RealizarEncuestaLenguajes extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class RealizarEncuestaLenguajes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.realizar_encuesta_lenguajes_activity);
+
+        MaterialButton siguiente = (MaterialButton) findViewById(R.id.siguiente);
+        siguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RealizarEncuestaLenguajes.this,
+                        RealizarEncuestaBasesDatos.class));
+            }
+        });
+
     }
 }
