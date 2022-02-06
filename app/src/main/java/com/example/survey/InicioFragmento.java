99f5295a4f888e48c482769fe55b2baf94cbd181
Ruben Arriazu <ruben.arsi@protonmail.ch>
2022-02-06 15:28:51 +0100

    Primer commit

---
+++ app/src/main/java/com/example/survey/InicioFragmento.java
@@ -0,0 +1,33 @@
+package com.example.survey;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.button.MaterialButton;
+
+public class InicioFragmento extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.inicio_fragmento, container, false);
+
+        MaterialButton botonComenzar = view.findViewById(R.id.comenzar);
+        botonComenzar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), RealizarEncuesta.class));
+            }
+        });
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/survey/RealizarEncuesta.java
@@ -0,0 +1,14 @@
+package com.example.survey;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RealizarEncuesta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.realizar_encuesta_activity);
+    }
+}
